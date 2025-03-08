481ac9fed1a62d1f0a6b36be4a2a1ec7d515aab1
bosta <bosta958@hotmail.fr>
2025-03-08 14:03:17 +0100

    Update CSP configuration

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(helmet({
       defaultSrc: ["'self'"],
       scriptSrc: ["'self'", "'unsafe-inline'"],
       styleSrc: ["'self'", "'unsafe-inline'"],
-      imgSrc: ["'self'", "data:", "https://res.cloudinary.com"],
+      imgSrc: ["'self'", "data:", "https:"],
       connectSrc: ["'self'", "https://api.stripe.com"],
       mediaSrc: ["'self'", "https://res.cloudinary.com"],
     },
